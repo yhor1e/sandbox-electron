66477a95250d5de7cb65708b6501fdc8cce609bf
yhorie <yuk.horie@gmail.com>
2016-10-23 01:48:47 +0900

    Practicing

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * https://github.com/electron/electron-quick-start
 * https://github.com/sindresorhus/electron-boilerplate
 * http://qiita.com/Quramy/items/a4be32769366cfe55778
-
+* https://github.com/electron/electron-api-demos
 
 ## note
 
--- my-electron/src/main.js
+++ my-electron/src/main.js
@@ -16,6 +16,9 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+  // for dev
+  mainWindow.webContents.openDevTools();
+
   mainWindow.on('closed', function() {
     mainWindow = null;  });
 });
